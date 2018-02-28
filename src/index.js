1e89473744e7d5b384791b603da25959c0beae7e
Alex Naidovich <alexnaidovich@users.noreply.github.com>
2018-02-28 22:34:30 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var temp = [];
+    
+    for (var i = 0, l = str.length; i < l; i++) {
+        for (var j = 0, le = bracketsConfig.length; j < le; j++) {
+            if (str[i] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]) {
+                if (temp.indexOf(str[i]) === -1) {
+                    temp.push(str[i]);
+                } else {
+                    if (temp[temp.length - 1] === bracketsConfig[j][0]) {
+                        temp.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            } else if (str[i] === bracketsConfig[j][0]) {
+                temp.push(str[i])
+            } else if (str[i] === bracketsConfig[j][1]) {
+                if (temp[temp.length - 1] === bracketsConfig[j][0]) {
+                    temp.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    
+    if (temp.length) {
+        return false;
+    } else {
+        return true;
+    }
 }
